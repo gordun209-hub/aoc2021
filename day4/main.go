9898dd54d5d4c7ba403cfc5997b7295b1b7fa61a
gordun209 <ali-han80@hotmail.com>
2022-10-24 16:17:01 +0300

    improvement

--- day4/main.go
+++ day4/main.go
@@ -6,61 +6,86 @@ import (
 	"strings"
 )
 
-const (
-	boardWidth    = 5
-	boardHeight   = 5
-	boardCapacity = 25
+type (
+	elem string
+	Row  struct {
+		elements []string
+		isMarked bool
+	}
+
+	Col struct {
+		elements []string
+		isMarked bool
+	}
+
+	Board struct {
+		rows  [5]Row
+		cols  [5]Col
+		isWon bool
+	}
 )
 
-type cell [2]string
-type row [4]cell
-type col [4]cell
-type Board struct {
-}
-
-var board = `
+var boardData = `
 22 13 17 11  0
  8  2 23  4 24
 21  9 14 16  7
  6 10  3 18  5
- 1 12 20 15 19
+1 12 20 15 19
 `
+var numbers = `7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1`
 
 func main() {
-
-	sl := strings.TrimSpace(board)
-	// splitted := strings.Split(sl, "\n")
-	// fmt.Printf("%v", splitted)
-	// rows := getRows(strings.Fields(sl))
-	cols := getCols(strings.Fields(sl))
-	fmt.Println(cols)
-
-}
-func getRows(data []string) []string {
-	newdata := []string{}
-	for i, v := range data {
+	var myboard = Board{}
+	sl := strings.TrimSpace(boardData)
+	sll := strings.Fields(sl)
+	for i := range sll {
 		if i < 5 {
-			fmt.Println(v)
-			newdata = append(newdata, v)
+
+			myboard.cols[i].elements = getCol(sll, i)
+			myboard.rows[i].elements = getRow(sll, i)
+			myboard.cols[i].isMarked = false
+			myboard.rows[i].isMarked = false
 		}
 	}
-	return newdata
+
+	numberss := strings.TrimSpace(numbers)
+	numbr := strings.Split(numberss, ",")
+	iswonn := checkIfWin(myboard, numbr)
+	fmt.Println(iswonn)
 
 }
-func getCols(data []string) []string {
+func checkIfWin(myboard Board, numbr []string) bool {
+	iswon := 0
+	for _, v := range numbr {
+		for i := 0; i < len(myboard.cols); i++ {
+			if v == myboard.cols[0].elements[i] {
+				iswon++
+				if iswon == 5 {
+					myboard.cols[0].isMarked = true
+
+				}
+			}
+		}
+	}
+	return iswon == 5
+}
+func getRow(data []string, index int) []string {
+	row := []string{}
+	for i := 0; i < 5; i++ {
+		row = append(row, data[i+5*index])
+	}
+	return row
+}
+
+func getCol(data []string, index int) []string {
 	newdata := []string{}
 	for i := 0; i < len(data); i += 5 {
-		fmt.Println(data[i])
-		newdata = append(newdata, data[i])
+		newdata = append(newdata, data[index+i])
 	}
 	return newdata
-}
-func standardizeSpaces(s string) string {
-	return strings.Join(strings.Fields(s), " ")
 }
 
 func getInput() string {
-
 	content, err := os.ReadFile("input")
 	if err != nil {
 		fmt.Println("err")
